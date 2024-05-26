d72f9ae20de041ec7f89f5c6b78109b039e6115c
Andrey Gruzdev <agruzdev1@gmail.com>
2024-05-26 16:16:07 +0200

    update contract

--- src/verified_contract/mod.rs
+++ src/verified_contract/mod.rs
@@ -1,11 +1,15 @@
 pub mod comment;
 pub mod vote;
 
-use near_sdk::near;
+use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::NearSchema;
 use std::collections::HashSet;
 use vote::Vote;
 
-#[near(serializers=[borsh, json])]
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, NearSchema)]
+#[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
 pub struct VerifiedContract {
     pub cid: String,
     pub lang: String,
